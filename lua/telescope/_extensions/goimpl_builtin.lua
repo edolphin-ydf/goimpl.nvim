f3eaac10436971d8b28de41ffa558246276a2937
edolphin <yangdongfeng@bituniverse.org>
2024-10-29 11:11:57 +0800

    support generic interface

--- lua/telescope/_extensions/goimpl_builtin.lua
+++ lua/telescope/_extensions/goimpl_builtin.lua
@@ -6,21 +6,50 @@
 -- Distributed under terms of the MIT license.
 --
 
-local actions = require'telescope.actions'
-local state = require'telescope.actions.state'
-local actions_set = require'telescope.actions.set'
-local conf = require'telescope.config'.values
-local finders = require'telescope.finders'
+local actions = require 'telescope.actions'
+local state = require 'telescope.actions.state'
+local actions_set = require 'telescope.actions.set'
+local conf = require 'telescope.config'.values
+local finders = require 'telescope.finders'
 local make_entry = require "telescope.make_entry"
-local pickers = require'telescope.pickers'
+local pickers = require 'telescope.pickers'
 local ts_utils = require 'nvim-treesitter.ts_utils'
 local channel = require("plenary.async.control").channel
+
+local function prequire(mod)
+	local ok, res = pcall(require, mod)
+	if ok then
+		return res
+	end
+	return nil
+end
+local plog = prequire("plenary.log")
+local logger
+if not plog then
+	local emptyFun = function(_) end
+	logger = {
+		trace = emptyFun,
+		debug = emptyFun,
+		info = emptyFun,
+		warn = emptyFun,
+		error = emptyFun,
+		fatal = emptyFun,
+	}
+else
+	logger = plog.new {
+		plugin = 'goimpl',
+		use_console = true,
+		use_file = true,
+	}
+end
+
+
 
 local ts = vim.treesitter
 local tsq = vim.treesitter.query
 
 local function _get_node_text(node, source, opts)
-  return (ts.get_node_text or tsq.get_node_text)(node, source, opts)
+	return (ts.get_node_text or tsq.get_node_text)(node, source, opts)
 end
 
 local M = {}
@@ -50,7 +79,7 @@ local function interfaces_to_items(symbols, bufnr)
 						lnum = range.start.line + 1,
 						col = range.start.character + 1,
 						kind = kind,
-						text = '['..kind..'] '..symbol.name,
+						text = '[' .. kind .. '] ' .. symbol.name,
 						containerName = symbol.containerName
 					})
 				end
@@ -62,7 +91,7 @@ local function interfaces_to_items(symbols, bufnr)
 						lnum = symbol.selectionRange.start.line + 1,
 						col = symbol.selectionRange.start.character + 1,
 						kind = kind,
-						text = '['..kind..'] '..symbol.name,
+						text = '[' .. kind .. '] ' .. symbol.name,
 						containerName = symbol.containerName
 					})
 				end
@@ -98,8 +127,8 @@ local function split(inputstr, sep)
 	if sep == nil then
 		sep = "%s"
 	end
-	local t={}
-	for str in string.gmatch(inputstr, "([^"..sep.."]+)") do
+	local t = {}
+	for str in string.gmatch(inputstr, "([^" .. sep .. "]+)") do
 		table.insert(t, str)
 	end
 	return t
@@ -119,14 +148,106 @@ local function handle_job_data(data)
 	return data
 end
 
-local function goimpl(tsnode, packageName, interface)
+
+local interface_declaration_query = vim.treesitter.query.parse("go", [[
+(type_declaration
+(type_spec
+  type: (interface_type))
+) @interface_declaration
+]])
+
+local generyc_type_name_parameters_query = vim.treesitter.query.parse("go", [[
+	[
+(type_spec
+  name: (type_identifier) @interface.generic.name
+  type_parameters: (type_parameter_list) @interface.generic.type_parameters
+  type: (interface_type) )
+]
+	]])
+
+local type_parameter_name_query = vim.treesitter.query.parse("go", [[(type_parameter_declaration
+  name: (identifier) @type_parameter_name)
+  ]])
+
+local function load_file_to_buffer(filepath, buf)
+	local file = io.open(filepath, "r")
+	if not file then
+		vim.notify("File not found: " .. filepath, vim.log.levels.WARN, { title = "goimpl.nvim" })
+		return false
+	end
+
+	local content = file:read("*all")
+	file:close()
+
+	-- 将内容写入缓冲区
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(content, "\n"))
+
+	return true
+end
+
+local function get_interface_generic_type_parameters(file, interface_name)
+	local buf = vim.api.nvim_create_buf(false, true)
+	local ok, errmsg = pcall(vim.api.nvim_set_option_value, "filetype", "go", { buf = buf })
+	if not ok then
+		local msg = ("Can't set filetype to 'go' (%s). Formatting is canceled"):format(errmsg)
+		vim.notify(msg, vim.log.levels.WARN, { title = "goimpl.nvim" })
+		return ""
+	end
+
+	if not load_file_to_buffer(file, buf) then
+		return ""
+	end
+
+	local parser = vim.treesitter.get_parser(buf, "go")
+	local tree = parser:parse()[1]
+	local root = tree:root()
+
+	for _, node, _ in interface_declaration_query:iter_captures(root, 0) do
+		local is_check_interface = false
+		for iid, inode, _ in generyc_type_name_parameters_query:iter_captures(node, 0) do
+			local name = generyc_type_name_parameters_query.captures[iid]
+			if name == "interface.generic.name" then
+				local current_interface_name = vim.treesitter.get_node_text(inode, 0)
+				if current_interface_name == interface_name then
+					is_check_interface = true
+				end
+			end
+		end
+
+		if is_check_interface then
+			for iid, inode, _ in generyc_type_name_parameters_query:iter_captures(node, 0) do
+				local name = generyc_type_name_parameters_query.captures[iid]
+				if name == "interface.generic.type_parameters" then
+					local type_parameter_names = {}
+					for _, tnode, _ in type_parameter_name_query:iter_captures(inode, 0) do
+						type_parameter_names[#type_parameter_names + 1] = vim.treesitter.get_node_text(tnode, 0)
+					end
+
+					return "[" .. table.concat(type_parameter_names, ", ") .. "]"
+				end
+			end
+		end
+	end
+
+	return ""
+end
+
+
+local function goimpl(tsnode, packageName, interface, type_parameter_list)
 	local rec2 = _get_node_text(tsnode, 0)
 	local rec1 = string.lower(string.sub(rec2, 1, 2))
 
 	-- get the package source directory
 	local dirname = vim.fn.fnameescape(vim.fn.expand('%:p:h'))
 
-	local setup = 'cd ' .. dirname .. ' && impl' .. ' -dir ' .. " '" .. dirname .. "' " .. " '" .. rec1 .. " *" .. rec2 .. "' " .. packageName .. '.' .. interface
+	local setup = 'cd ' ..
+		dirname ..
+		' && impl' ..
+		' -dir ' ..
+		" '" ..
+		dirname ..
+		"' " .. " '" .. rec1 .. " *" .. rec2 .. "' '" .. packageName .. '.' .. interface .. type_parameter_list .. "'"
+	logger.debug(setup)
 	local data = vim.fn.systemlist(setup)
 
 	data = handle_job_data(data)
@@ -137,7 +258,9 @@ local function goimpl(tsnode, packageName, interface)
 	-- if not found the '$packageName.$interface' type, then try without the packageName
 	-- this works when in a main package, it's containerName will return the directory name which the interface file exist in.
 	if string.find(data[1], "unrecognized interface:") or string.find(data[1], "couldn't find") then
-		setup = 'impl' .. ' -dir ' .. dirname  .. " '" .. rec1 .. " *" .. rec2 .. "' " .. interface
+		setup = 'impl' ..
+		' -dir ' .. dirname .. " '" .. rec1 .. " *" .. rec2 .. "' '" .. interface .. type_parameter_list .. "'"
+		logger.debug(setup)
 		data = vim.fn.systemlist(setup)
 
 		data = handle_job_data(data)
@@ -147,9 +270,9 @@ local function goimpl(tsnode, packageName, interface)
 	end
 
 	local _, _, pos, _ = tsnode:parent():parent():range()
-	pos = pos+1
+	pos = pos + 1
 	vim.fn.append(pos, "") -- insert an empty line
-	pos = pos+1
+	pos = pos + 1
 	vim.fn.append(pos, data)
 end
 
@@ -160,7 +283,7 @@ M.goimpl = function(opts)
 	local tsnode = ts_utils.get_node_at_cursor()
 	if tsnode:type() ~= 'type_identifier' or tsnode:parent():type() ~= 'type_spec'
 		or tsnode:parent():parent():type() ~= 'type_declaration' then
-		print("No type identifier found under cursor")
+		logger.info("No type identifier found under cursor")
 		return
 	end
 
@@ -185,7 +308,9 @@ M.goimpl = function(opts)
 				local symbol_name = split(entry.symbol_name, ".")
 				symbol_name = symbol_name[#symbol_name]
 
-				goimpl(tsnode, entry.value.containerName, symbol_name)
+				local type_parameter_list = get_interface_generic_type_parameters(entry.filename, symbol_name)
+
+				goimpl(tsnode, entry.value.containerName, symbol_name, type_parameter_list)
 			end)
 			return true
 		end,
